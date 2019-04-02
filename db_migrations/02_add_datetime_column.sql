9305ddcf4cbf3c0192716c4411d7b121acab3b7d
karelmolina <edmolina4@gmail.com>
2019-04-01 21:56:41 -0600

    As a maker i wnat to see all peeps in reverse chronological order
    rspec problem non solved. pg gem

---
+++ db_migrations/02_add_datetime_column.sql
@@ -0,0 +1,1 @@
+Alter table peeps add column datetime timestamp DEFAULT CURRENT_TIMESTAMP;
--- lib/peep.rb
+++ lib/peep.rb
@@ -3,24 +3,27 @@ require_relative './dbconnect'
 class Peep
 
   def self.show
-    res = DbConnect.query("SELECT * FROM peeps;")
+    res = DbConnect.query("SELECT * FROM peeps ORDER BY datetime DESC;")
     res.map do |peep|
       Peep.new(id: peep["id"],
-              content: peep["content"])
+              content: peep["content"],
+              date: peep["datetime"])
     end
 
   end
 
   def self.create(content:)
-    res = DbConnect.query("INSERT INTO peeps (content) VALUES ('#{content}') RETURNING id, content;")
+    res = DbConnect.query("INSERT INTO peeps (content) VALUES ('#{content}') RETURNING id, content, datetime;")
     Peep.new(id: res[0]["id"],
-            content: res[0]["content"])
+            content: res[0]["content"],
+            date: res[0]["datetime"])
   end
 
-  attr_reader :id, :content
+  attr_reader :id, :content, :date
 
-  def initialize(id:, content:)
+  def initialize(id:, content:, date:)
     @id = id
     @content = content
+    @date = date
   end
 end
